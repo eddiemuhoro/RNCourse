7fb24a3d30b2768be31c21af6ba4a388c5843a59
eddiemuhoro <eddiemuhoro@gmail.com>
2023-04-15 15:21:15 +0300

    hide dashboard header

--- App.js
+++ App.js
@@ -8,28 +8,31 @@ import ProductList from './app/ProductList';
 import ProductDetailsScreen from './app/ProductDetailsScreen';
 import BottomNavigation from './app/BottomNavigationBar';
 import RelatedProducts from './app/RelatedProducts';
+import { View } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function App() {
  
   return (
+   
+
+  
     <NavigationContainer
     //set vertical scroll to true
     verticalScrollEnabled={true}
     >
       <Stack.Navigator >
         {/* <Stack.Screen name="Login" component={Login} /> */}
-        <Stack.Screen name="Shop" component={Dashboard} />
+        <Stack.Screen name="Dashboard"  options={{ headerShown: false }} component={Dashboard} />
          <Stack.Screen name="ProductList" component={ProductList} />
         <Stack.Screen name="ProductDetails" component={ProductDetailsScreen} />
         <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
         <Stack.Screen name="RelatedProducts" component={RelatedProducts} />
-        
-        
       </Stack.Navigator>
-      
+   
 
   </NavigationContainer>
+
   );
 }
