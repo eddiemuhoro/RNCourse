115e652e4c562842139fe5605d342cb1090ae295
eddiemuhoro <eddiemuhoro@gmail.com>
2023-04-15 15:22:57 +0300

    changed title to bold

--- app/ProductDetailsScreen.js
+++ app/ProductDetailsScreen.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, ScrollView, TouchableOpacity } from 'react-native';
 import RelatedProducts from './RelatedProducts';
+import { Dimensions } from 'react-native';
+import axios from 'axios';
 
 const ProductDetailsScreen = ({ route }) => {
   const { productId } = route.params;
@@ -37,10 +39,12 @@ const ProductDetailsScreen = ({ route }) => {
       <Button style={{width:'100%'}} title="Add to Cart" onPress={() => {}} />
 
       <View>
-        <Text>Related Products</Text>
-        <RelatedProducts />
+        <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 10 }}>Related Products</Text>
+        <RelatedProducts  />
       </View>
     </View>
+  
+   
   );
 };
 
@@ -48,6 +52,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    minHeight: Dimensions.get('window').height + 100, // add 100 for the related products section
+
   },
   productImage: {
     width: '100%',
