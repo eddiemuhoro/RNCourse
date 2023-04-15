09c82e0fe65d80947f8940341c462244d0e934a4
eddiemuhoro <eddiemuhoro@gmail.com>
2023-04-15 15:21:47 +0300

    added tab states

--- app/PROFILE/MyProfile.js
+++ app/PROFILE/MyProfile.js
@@ -1,44 +1,88 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Image, StyleSheet, Text, View, TouchableOpacity, ScrollView, FlatList } from "react-native";
+import ProductList from "../ProductList";
 
 const MyProfile = () =>{
 
-    
+    const [orders, setOrders] = useState(false)
+   
+    const [wishlist, setWishlist] = useState(false)
+    const [posts, setPosts] = useState(true)
+
+
+    const handleOrders = () => {
+        setOrders(true)
+        setWishlist(false)
+        setPosts(false)
+   
+    }
+
+ 
+
+    const handleWishList = () => {
+        setOrders(false)
+        setWishlist(true)
+        setPosts(false)
+    }
+
+    const handlePosts = () => {
+        setOrders(false)
+        setWishlist(false)
+        setPosts(true)
+    }
+
+    console.log(posts, orders, wishlist)
+
 
     return(
-        <View>
-
-        
-        <View style={styles.profileContainer}>
-            <Image 
+        <FlatList
+       // your key extractor function
+        ListHeaderComponent={
+          <View>
+            <View style={styles.profileContainer}>
+              <Image 
                 style={styles.profileImage}
                 source={{uri : 'https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg'}}
-            />
-            <View>
+              />
+              <View>
                 <Text style={styles.profileName}>Edwin Mwangi</Text>
                 <Text>eddiemuhoro@gmail.com</Text>
                 <Text>0705947364</Text>
                 <Text>followers: 1</Text>
+              </View>
             </View>
-        </View>
-
-        {/* tabs for my profile screen including my posts, my followers, my following */}
-        <View style={styles.profileTabs}>
-            <View>
-                <Text>My Posts</Text>
+            
+            <View style={styles.profileTabs}>
+              <TouchableOpacity style={posts ? styles.activeButton : styles.button} onPress={handlePosts}>
+                <Text style={styles.buttonText}>My Posts</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={orders ? styles.activeButton : styles.button} onPress={handleOrders} >
+                <Text style={styles.buttonText}>My Orders</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={wishlist ? styles.activeButton : styles.button} onPress={handleWishList}>
+                <Text style={styles.buttonText}>My Wishlist</Text>
+              </TouchableOpacity>
             </View>
 
-            <View>
-                <Text>My Followers</Text>
-            </View>
 
-            <View>
-                <Text>My Following</Text>
-            </View>
-                 
-        </View>
+            {
+                posts ? <ProductList /> : null
+            }
+            {
+                orders ? <Text>No Orders yet</Text> : null
+            }
+            {
+                wishlist ? <Text>No Wishlist yet</Text> : null
+            }
+          
 
-        </View>
+          </View>
+        }
+        ListFooterComponent={null}
+        contentContainerStyle={{flexGrow: 1}}
+        style={{height:'100%'}}
+      />
+      
     )
 }
 
@@ -72,7 +116,28 @@ const styles = StyleSheet.create({
             alignItems:"center",
             marginVertical: 10,
             marginHorizontal: 20,
-        }
+        },
+        button: {
+            backgroundColor: 'treansparent',
+            padding: 10,
+            borderRadius: 5,
+          
+           
+            marginVertical: 10,
+          },
+          buttonText: {
+            color: 'gray',
+            fontSize: 16,
+            fontWeight: 'bold',
+          },
+          activeButton : {
+            backgroundColor: 'white',
+            padding: 10,
+            borderRadius: 5,
+            borderWidth: 1,
+            borderColor: 'gray',
+            
+          }
 
 })
 
